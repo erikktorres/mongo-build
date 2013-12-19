3e2a28b0282a80df413340f83514e92d25fc9e5b
Eric Tschetter <cheddar@tidepool.org>
2013-12-19 11:27:09 -0600

    Make the start script better

--- start.sh
+++ start.sh
@@ -1,3 +1,10 @@
 #! /bin/bash -eu
 
+DATA_DIR=`cat config/config.conf | grep "^dbpath" | cut -d '=' -f 2-`
+if [ ! -d ${DATA_DIR} ];
+then
+  echo "Creating data dir[${DATA_DIR}]"
+  mkdir -p ${DATA_DIR}
+fi
+
 exec bin/mongod --config config/config.conf
