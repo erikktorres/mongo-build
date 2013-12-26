362692f324aba5a8c83462bc1c6d3c25609d291d
Eric Tschetter <cheddar@tidepool.org>
2013-12-26 16:45:10 -0600

    Stop things better

--- watcher.js
+++ watcher.js
@@ -1,16 +1,41 @@
+var async = require('async');
+
+var log = require('./lib/log.js')('watcher.js');
 
 (function(){
   'use strict'
 
   var config = require('./env.js');
 
-  require('./lib/mongo/runner.js')(config.mongo).start();
-
-  require('./lib/mongoBackup.js')(
+  var mongoRunner = require('./lib/mongo/runner.js')(config.mongo);
+  var mongoBackup = require('./lib/mongoBackup.js')(
     require('./lib/mongo/client')(config.mongo),
     require('./lib/init/pusher.js')(config.pusher()),
     require('./lib/common/fileUtils.js'),
     require('./lib/common/polling.js'),
     config.backup
-  ).start();
+  )
+
+  async.series([
+    mongoRunner.start.bind(mongoRunner),
+    mongoBackup.start.bind(mongoBackup)
+  ],
+  function(err){
+    if (err != null) {
+      log.error(err, 'Problem starting up.  Stopping!');
+      process.emit('SIGTERM');
+    }
+  });
+
+  process.on('SIGTERM', function(){
+    async.series([
+      mongoBackup.stop.bind(mongoBackup),
+      mongoRunner.stop.bind(mongoRunner)
+    ],
+    function(err) {
+      if (err != null) {
+        log.error(err, 'Problem shutting down!?  Oh well.');
+      }
+    });
+  });
 })();
